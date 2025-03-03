10162351e45b5300746913d9b0e9d5db1ac54654
Tyler Allen <tyler.allen@hashicorp.com>
2025-03-03 21:27:13 +0000

    lets use private providers

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform { 
+  required_providers {
+    random = {
+      source = "app.terraform.io/tallen-demo/random"
+      version = "3.7.1"
+    }
+  }
+}
+
 resource "random_integer" "demo" {
   min = 1
   max = 100
